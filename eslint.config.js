2f3262fd878876744439b95f83c82c1f2d1a0149
Benjamin <bens@spreadshirt.net>
2025-01-14 22:07:11 +0100

    most eslint plugins work well and use expected rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,9 @@
-import { fixupConfigRules } from "@eslint/compat";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: [
@@ -27,53 +16,22 @@ export default [
       "eslint-script-rules-overview.js",
     ],
   },
-
-  ...fixupConfigRules(compat.extends("eslint:recommended")),
-  ...fixupConfigRules(
-    compat.extends(
-      "plugin:@typescript-eslint/strict-type-checked",
-      "plugin:@typescript-eslint/stylistic-type-checked",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:react/jsx-runtime",
-    ),
-  ),
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
-    files: ["**/*.ts", "**/*.tsx"],
-
-    plugins: {
-      "react-refresh": reactRefresh,
-    },
-
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-
       parserOptions: {
         projectService: true,
-        project: ["./tsconfig.json", "./tsconfig.node.json"],
-        tsconfigRootDir: __dirname,
+        tsconfigRootDir: import.meta.dirname,
       },
-    },
-
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-
-    rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        {
-          allowConstantExport: true,
-        },
-      ],
     },
   },
+  //   todo 'react/react-in-jsx-scope' to be disabled
+  pluginReact.configs.flat.recommended,
+  //   todo enable
+  // react-hooks/exhaustive-deps
+  // react-refresh/only-export-components
 ];
